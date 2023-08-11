54ee5c0ebfd570118ce3ae77cb5f61c2ea766c94
kwinte17 <kylevwinters@gmail.com>
2023-08-11 12:04:40 -0600

    First successful prediction

--- Predict.py
+++ Predict.py
@@ -2,6 +2,7 @@
 import time
 print('->Begin Imports')
 tic = time.time()
+from common.types import *
 from sklearn.cluster import DBSCAN
 from PIL import Image, ImageOps
 import os
@@ -19,7 +20,6 @@ with open(MODEL_PATH, 'r') as f:
     for line in f:
         line_list = line.strip().split(',')
         model[line_list[0]] = np.array(list(map(float, line_list[1:])))
-import pdb; pdb.set_trace()
 
 print('->Begin Loading Image')
 tic = time.time()
@@ -28,4 +28,30 @@ im = ImageOps.grayscale(Image.open(PREDICTION_IMAGE))
 img_array = np.asarray(im)
 print(f'->End Loading Image ({time.time() - tic} seconds)')
 
+print('->Begin Clustering Letter Pixels')
+tic = time.time()
+# Create a boolean map
+bool_array = np.zeros(np.shape(img_array))
+bool_array[img_array < np.max(img_array)] = 1
+
+# Assemble True pixels into single variable
+X = np.transpose(np.asarray(img_array < np.max(img_array)).nonzero())
+
+# Cluster using DBSCAN
+db = DBSCAN(eps=4, min_samples=3).fit(X)
+labels = db.labels_
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+print('Number of clusters found = ', n_clusters_)
+print(f'->End Clustering Letter Pixels ({time.time() - tic} seconds)')
+
+# Perform prediction
+for ii, l in enumerate(set(labels)):
+    err = np.zeros(len(model))
+    X_l = X[labels == l, :]
+    q = letter_pixels(X_l)
+    centers = q.get_encoded_centers()
+    for jj, category in enumerate(model):
+        err[jj] = np.linalg.norm(model[category] - centers)
+    letters_ordered_by_probability = np.array([key for key in model.keys()])[np.argsort(err)]
+    print(f'Prediction: "{letters_ordered_by_probability[0]}"')
 
